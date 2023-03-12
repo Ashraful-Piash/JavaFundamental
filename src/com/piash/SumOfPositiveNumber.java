c334b38f33d7515196aef8c2b78ae49fed2e64da
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-03-12 21:52:31 +0600

    Created SumOfPositive Number

---
+++ src/com/piash/SumOfPositiveNumber.java
@@ -0,0 +1,16 @@
+package com.piash;
+
+import java.util.Scanner;
+
+public class SumOfPositiveNumber {
+    public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int sum = 0;
+    int num;
+    do {
+        num = sc.nextInt();
+        sum += num;
+    } while(num >= 0);
+    System.out.println("Sum is : " +sum);
+    }
+}
