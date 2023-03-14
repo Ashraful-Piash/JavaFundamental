8ef9f60b07cf08a18583d9ed561e53362a914923
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-03-15 00:43:42 +0600

    Updated

--- src/com/piash/SumOfPositiveNumber.java
+++ src/com/piash/SumOfPositiveNumber.java
@@ -1,18 +1,18 @@
 package com.piash;
 
 import java.util.Scanner;
-
+//Given a stream of number. Read the numbers till you find a -ve(negative) number. And print their sum of numbers read so far
 public class SumOfPositiveNumber {
-    public static void main(String[]args){
-    Scanner sc = new Scanner(System.in);
-    int sum = 0;
-    int num;
-    do {
-        num = sc.nextInt();
-        sum += num;
-        //done
-        //tested
-    } while(num >= 0);
-    System.out.println("Sum is : " +sum);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int num;
+
+        do{
+            num = sc.nextInt();
+            sum += num;
+        }while (num>=0);
+        System.out.println("sum :" +sum);
     }
+    
 }
