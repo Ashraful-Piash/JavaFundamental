72e54b31bb55bcb64f1bfd999cb523f7b299aad6
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-03-19 14:17:14 +0600

    Modified

--- src/com/piash/ArrayOne.java
+++ src/com/piash/ArrayOne.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class ArrayOne {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] arr = new int[6];
+        int n = sc.nextInt();
+        int [] arr = new int[n];
         for(int i = 0; i< arr.length; i++){
                 arr [i] = sc.nextInt();
         }
-        System.out.println(arr[0]);
-        System.out.println(arr[3]);
-
+        for(int i = 0; i<arr.length; i ++ ){
+            System.out.println("At index " + i + " Value: " + arr[i]);
+        }
     }
 }
